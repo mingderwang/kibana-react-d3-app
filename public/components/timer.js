ac81de5a57b950afdb9d9e8293d2064b57b1d236
Ming-der Wang <ming@log4analytics.com>
2017-04-13 17:37:53 +0800

    add mapDispatchToProps

--- public/components/timer.js
+++ public/components/timer.js
@@ -7,33 +7,45 @@ const oneUp = createAction('INCREMENT');
 const twoUp = createAction('ADDTWO');
 
 const Counter = React.createClass({
-  addOne() {
-    const { dispatch } = this.props;
-    dispatch(oneUp());
-  },
-  addTwo() {
-    const { dispatch } = this.props;
-    dispatch(twoUp());
-  },
   render() {
     return (
   <div>
     <h1>結果 = {this.props.value}</h1>
-    <button onClick={this.addOne}> +1 </button>
-    <button onClick={this.addTwo}> +2 </button>
+    <button onClick={this.props.onIncrement}> +1 </button>
+    <button onClick={this.props.onDecrement}> +2 </button>
   </div>
     );
   }
 });
 
-const mapStateToProps = (state) => {
+/*
+ * 對應 props 到 dispatch 哪個 action
+ */
+const mapDispatchToProps = (dispatch) => {
   return {
-    value: state.value
+    onIncrement: () => {
+      dispatch({
+        type: 'INCREMENT'
+      })
+    },
+    onDecrement: () => {
+      dispatch({
+        type: 'ADDTWO'
+      })
+    }
+  };
+};
+
+const mapStateToProps = (...args) => {
+  console.log(args[0]); // state
+  return {
+    value: args[0].value
   };
 };
 
 const Timer = connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Counter);
 
 export default Timer
