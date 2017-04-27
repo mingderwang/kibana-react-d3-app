3b544124595ba02aea90de35147f7edac3f30f5c
Ming-der Wang <ming@log4analytics.com>
2017-04-27 13:02:47 +0800

    add Commends list

--- public/components/app.js
+++ public/components/app.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import Chart from './chart';
 
+let comments = [
+    {id: 1, author: "Cory Brown", text: "My 2 scents"},
+    {id: 2, author: "Jared Anderson", text: "Let me put it this way. You've heard of Socrates? Aristotle? Plato? Morons!"},
+    {id: 3, author: "Matt Poulson", text: "It's just a function!"},
+    {id: 4, author: "Bruce Campbell", text: "Fish in a tree? How can that be?"}
+];
+
+const CommentList = (props) => (
+    <div className="commentList">
+        { props.comments.map(comment => (
+            <Comment author={ comment.author } key={comment.id} >
+                { comment.text }
+            </Comment>
+        )) }
+    </div>
+);
+
 const Comment = (props) => (
    <div className="comment">
        <h2 className="commentAuthor">
@@ -14,9 +31,7 @@ export default React.createClass({
   render() {
     return (
       <div className='react--main'>
-      <Comment author="Cory">
-      So, what do you think so far?
-  </Comment>
+      <CommentList comments={ comments } />
       </div>
     );
   }
