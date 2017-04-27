9ab6e45013e1db10f223d2272465990dd35978c2
Ming-der Wang <ming@log4analytics.com>
2017-04-27 12:57:17 +0800

    tutorial code https://medium.com/front-end-developers/react-redux-tutorial-d1f6c6652759

--- public/components/app.js
+++ public/components/app.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Chart from './chart';
 
+const Comment = (props) => (
+   <div className="comment">
+       <h2 className="commentAuthor">
+           { props.author }
+       </h2>
+       { props.children }
+   </div>
+ );
+
 export default React.createClass({
   render() {
     return (
       <div className='react--main'>
-        <Chart/>
+      <Comment author="Cory">
+      So, what do you think so far?
+  </Comment>
       </div>
     );
   }
